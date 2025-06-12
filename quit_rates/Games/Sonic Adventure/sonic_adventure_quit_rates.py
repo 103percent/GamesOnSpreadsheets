6161e92d66716e9659c5ba0c53659840c65b824f
Jak Marshall <jkmrshll88@gmail.com>
2025-06-12 16:50:36 +0100

    Sonic Adventure Quit Rates

--- quit_rates/Games/Sonic Adventure/sonic_adventure_quit_rates.py
+++ quit_rates/Games/Sonic Adventure/sonic_adventure_quit_rates.py
@@ -7,7 +7,7 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import matplotlib.image as mpimg
 
 # Optional: Load data from Excel
-df = pd.read_excel("yakuza_0_quit_rates.xlsx")
+df = pd.read_excel("sonic_adventure_quit_rates.xlsx")
 
 # Set Seaborn style
 sns.set_theme(style="whitegrid")
@@ -17,13 +17,13 @@ fig, ax = plt.subplots(figsize=(14, 10))
 plt.gca().yaxis.set_major_formatter(PercentFormatter(xmax=1, decimals=0))  # xmax=1 assumes y is in 0-1
 
 # List of indexes to highlight (e.g., 5th, 10th, 15th points)
-highlight_indexes = [0,8]
+highlight_indexes = [1,5,6]
 
 # Line plot
 sns.lineplot(data=df, x='chapter_name', y='quit_rate_pc', marker='o', linewidth=2.5, color='#007acc')
 ax.yaxis.set_major_formatter(PercentFormatter(xmax=1))
-ax.set_title("Yakuza 0 - Quit Rates", fontsize=18, weight='bold')
-ax.set_xlabel("Chapter", fontsize=14)
+ax.set_title("Sonic Adventure - Quit Rates", fontsize=18, weight='bold')
+ax.set_xlabel("Trophy", fontsize=14)
 ax.set_ylabel("Quit Rate", fontsize=14)
 plt.xticks(rotation=90)
 plt.grid(True, linestyle='--', alpha=0.6)
@@ -54,7 +54,7 @@ for idx in highlight_indexes:
 img = mpimg.imread("image.png")
 imagebox = OffsetImage(img, zoom=0.6)  # Adjust zoom as needed
 ab = AnnotationBbox(imagebox, (1, 1), xycoords='axes fraction', frameon=False,
-                    box_alignment=(1.1, 1.1))  # Push it just outside the top right
+                    box_alignment=(2.1, 1.1))  # Push it just outside the top right
 ax.add_artist(ab)
 
 # Show Plot
